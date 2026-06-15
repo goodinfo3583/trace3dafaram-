c7eb3e5717b5759aa006e8a72c63b8f0680cd396
goodinfo3583 <fly3203411@gmail.com>
2026-06-15 13:51:33 +0800

    Update test_web.py

--- test_web.py
+++ test_web.py
@@ -183,7 +183,7 @@ marquee_code = f"""
 </div>
 """
 # 渲染到網頁上
-slideshow_code = f""" ... """  # 將定義名稱也改為 slideshow_code
+st.markdown(marquee_code, unsafe_allow_html=True)
 #
 # ==========================================
 # 🛑 隱形急救引擎 (請置於程式最頂端，絕對不要刪除！)
