a4d97c3e45c6381aab4a6b86a036fe70d25988be
goodinfo3583 <fly3203411@gmail.com>
2026-06-15 16:41:55 +0800

    Update test_web.py

--- test_web.py
+++ test_web.py
@@ -30,20 +30,20 @@ st.markdown("""
 """, unsafe_allow_html=True)
 #置頂預留縮排
 #除錯紅色框
-st.markdown("""
-    <style>
-    /* 給所有主要的區塊加上紅色邊框 */
-    div[data-testid="stVerticalBlock"] {
-        border: 2px solid red !important;
-    }
-    </style>
-""", unsafe_allow_html=True)
+#st.markdown("""
+    #<style>
+    #/* 給所有主要的區塊加上紅色邊框 */
+    #div[data-testid="stVerticalBlock"] {
+        #border: 2px solid red !important;
+    #}
+    #</style>
+#""", unsafe_allow_html=True)
 #除錯紅色框
 st.markdown("""
     <style>
     /* 縮小頁面頂部的留白 */
     .block-container {
-        padding-top: 1rem; /* 預設通常是 3rem，您可以調整成 0.5rem 或 0rem */
+        padding-top: 0rem; /* 預設通常是 3rem，您可以調整成 0.5rem 或 0rem */
     }
     </style>
 """, unsafe_allow_html=True)
