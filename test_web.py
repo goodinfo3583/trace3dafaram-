3cec93483a5a168d0b1f2f6a4d4b1a3232802670
goodinfo3583 <fly3203411@gmail.com>
2026-06-15 12:59:52 +0800

    Update test_web.py

--- test_web.py
+++ test_web.py
@@ -112,7 +112,7 @@ bg_path = os.path.join(IMAGE_DIR, "派對盛宴邀請.png")
 set_background(bg_path)
 #跑馬燈
 # 圖片檔名列表
-image_files = ["跑馬燈1.png", "跑馬燈2.png", "跑馬燈3.png"]
+image_files = ["跑馬燈1.PNG", "跑馬燈2.PNG", "跑馬燈3.PNG"]
 
 # 生成圖片標籤的 HTML
 image_tags = ""
