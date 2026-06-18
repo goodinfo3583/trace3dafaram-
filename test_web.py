76198568b7c932a92ea6754396667d29955d8bf6
goodinfo3583 <fly3203411@gmail.com>
2026-06-18 23:03:23 +0800

    Update test_web.py

--- test_web.py
+++ test_web.py
@@ -558,14 +558,14 @@ if (!parentDoc.getElementById('custom-sticky-header')) {
         </div>
         <div class="nav-btn-container" id="nav-btn-container">
             <a href="#" id="custom-sidebar-toggle" class="nav-text-link">📂 呼叫側邊欄</a><span class="nav-divider">|</span>
-            <a href="#" data-target="NavToNews" class="nav-text-link internal-nav">☕ 市場消息</a><span class="nav-divider">|</span>
-            <a href="#" data-target="NavToPool" class="nav-text-link internal-nav">⛲ 觀察名單</a><span class="nav-divider">|</span>
-            <a href="#" data-target="NavToB1" class="nav-text-link internal-nav">👑 法人持股</a><span class="nav-divider">|</span>
-            <a href="#" data-target="NavToB2" class="nav-text-link internal-nav">🎯 法人掃貨</a><span class="nav-divider">|</span>
-            <a href="#" data-target="NavToB3" class="nav-text-link internal-nav">📅 法人連買</a><span class="nav-divider">|</span>
-            <a href="#" data-target="NavToB4" class="nav-text-link internal-nav">🔄 資券軋空</a><span class="nav-divider">|</span>
+            <a href="#" data-target="NavToNews" class="nav-text-link internal-nav">◆ 市場消息</a><span class="nav-divider">|</span>
+            <a href="#" data-target="NavToPool" class="nav-text-link internal-nav">◆ 觀察名單</a><span class="nav-divider">|</span>
+            <a href="#" data-target="NavToB1" class="nav-text-link internal-nav">◆ 法人持股</a><span class="nav-divider">|</span>
+            <a href="#" data-target="NavToB2" class="nav-text-link internal-nav">◆ 法人掃貨</a><span class="nav-divider">|</span>
+            <a href="#" data-target="NavToB3" class="nav-text-link internal-nav">◆ 法人連買</a><span class="nav-divider">|</span>
+            <a href="#" data-target="NavToB4" class="nav-text-link internal-nav">◆ 資券軋空</a><span class="nav-divider">|</span>
             <a href="#" data-target="NavToB5" class="nav-text-link internal-nav"><img src="app/static/wirtleg.png" class="nav-icon" alt="icon">大腿動向</a><span class="nav-divider">|</span>
-            <a href="#" data-target="NavToB6" class="nav-text-link internal-nav">🎣 鉅額交易</a>
+            <a href="#" data-target="NavToB6" class="nav-text-link internal-nav">◆ 鉅額交易</a>
         </div>
     `;
     parentDoc.body.insertBefore(headerDiv, parentDoc.body.firstChild);
