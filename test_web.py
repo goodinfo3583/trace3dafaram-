f5674c06d5e14cc3921596095abba1cf3de26bbe
goodinfo3583 <fly3203411@gmail.com>
2026-06-19 12:27:30 +0800

    Update test_web.py

--- test_web.py
+++ test_web.py
@@ -559,7 +559,7 @@ if (!parentDoc.getElementById('custom-sticky-header')) {
         <div class="nav-btn-container" id="nav-btn-container">
             <a href="#" id="custom-sidebar-toggle" class="nav-text-link">📂 呼叫側邊欄</a><span class="nav-divider">|</span>
             <a href="#" data-target="NavToNews" class="nav-text-link internal-nav"><img src="app/static/magicbook2.png" class="nav-icon" alt="icon">市場消息</a><span class="nav-divider">|</span>
-            <a href="#" data-target="NavToPool" class="nav-text-link internal-nav"><img src="app/static/jewllove.png" class="nav-icon" alt="icon">觀察名單</a><span class="nav-divider">|</span>
+            <a href="#" data-target="NavToPool" class="nav-text-link internal-nav"><img src="app/static/magicbookfire2.png" class="nav-icon" alt="icon">觀察名單</a><span class="nav-divider">|</span>
             <a href="#" data-target="NavToB1" class="nav-text-link internal-nav"><img src="app/static/magicbookleaf.png" class="nav-icon" alt="icon">法人持股</a><span class="nav-divider">|</span>
             <a href="#" data-target="NavToB2" class="nav-text-link internal-nav"><img src="app/static/magicbookwind.png" class="nav-icon" alt="icon">法人掃貨</a><span class="nav-divider">|</span>
             <a href="#" data-target="NavToB3" class="nav-text-link internal-nav"><img src="app/static/magicbookwater.png" class="nav-icon" alt="icon">法人連買</a><span class="nav-divider">|</span>
