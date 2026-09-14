1698dbd67a293d9289bdca2554960d3ddfa26d04
goodinfo3583 <fly3203411@gmail.com>
2026-09-14 10:09:20 +0800

    Update setting.py

--- views/setting.py
+++ views/setting.py
@@ -49,7 +49,7 @@ def render():
         new_hotkeys["NavToB7"] = st.text_input("董監動向", value=reverse_map.get("NavToB7", "F8"), key="kb7")
     with col2:   
         new_hotkeys["NavToWatchlist"] = st.text_input("自選名單", value=reverse_map.get("NavToWatchlist", "Alt+L"), key="kb_wl")
-        new_hotkeys["NavToWeightBacktest"] = st.text_input("籌碼過濾", value=reverse_map.get("籌碼過濾", "Alt+Q"), key="kb_WeightBacktest")
+        new_hotkeys["NavToWeightBacktest"] = st.text_input("籌碼過濾", value=reverse_map.get("NavToWeightBacktest", "Alt+Q"), key="kb_WeightBacktest")
         new_hotkeys["登入"] = st.text_input("登入", value=reverse_map.get("登入", "escape"), key="kb_login")
         new_hotkeys["設置"] = st.text_input("設置", value=reverse_map.get("設置", "F12"), key="kb_set")
 
